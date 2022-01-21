6c1acbd67b666a5e0a5f4b4a3a0e1b1696c09a7d
Zihad550 <hussainmuzahid83@gmail.com>
2022-01-21 17:30:54 +0600

    updated

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-// end of the day react reutrns a function in a incapsulated way
 class Clock extends React.Component {
-  // react will automatically call the render method
-  // never update this.props inside a class component
-  // because when a props changes react re-renders the component
   render() {
     return (
       <h1>
@@ -16,7 +12,6 @@ class Clock extends React.Component {
   }
 }
 
-//  when i will send a element to reactdom.render then it will create a virtual dom with the new elment
 ReactDom.render(
   <Clock locale="bn-BD"> Testing </Clock>,
   document.getElementById("root")
